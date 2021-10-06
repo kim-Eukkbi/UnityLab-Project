4ede88cbdcd11afcece94aa80d0c31cbad82e3b4
Eukkbi <smbaekr56@naver.com>
2021-10-06 19:40:36 +0900

    자이로 센서를 사용하여 플레이어 완성

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -7,6 +7,7 @@ public class PlayerInput : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    public float sensitivity;
 
     private Rigidbody rid;
     private int jumpCount =0;
@@ -14,10 +15,15 @@ public class PlayerInput : MonoBehaviour
     public void Start()
     {
         rid = gameObject.GetComponent<Rigidbody>();
+        Input.gyro.enabled = true;
     }
     public void Update()
     {
-        transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+        Debug.Log(Input.gyro.attitude);
+        //var gyroMoveA = Input.gyro.attitude;
+        var gyroMoveR = Input.gyro.rotationRateUnbiased;
+        transform.position = new Vector3(transform.position.x + gyroMoveR.y * sensitivity, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
 
         if(Input.GetMouseButtonDown(0))
         {
