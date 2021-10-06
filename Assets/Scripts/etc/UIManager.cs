cce8ae267b8e3ca96357dab6c22d432ebeebccf0
DESKTOP-G0KD87B\juhye <juhyeong_noteBook>
2021-10-06 19:04:48 +0900

    제너릭 싱글톤

--- Assets/Scripts/etc/UIManager.cs
+++ Assets/Scripts/etc/UIManager.cs
@@ -1,35 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class UIManager : MonoBehaviour
+public class UIManager : GenericSingleton<UIManager>
 {
     public CanvasGroup gameoverPanel;
 
-    public static UIManager instance;
+    [SerializeField] private Button fastStartButton;
 
-
-    private void Awake()
-    {
-        if (instance != null)
-        {
-            Debug.Log("다수의 게임매니저가 실행중입니다");
-        }
-        instance = this;
-    }
 
     void Start()
     {
+        fastStartButton.onClick.AddListener(() => SceneManager.LoadScene("Loading"));
+
         gameoverPanel.interactable = false;
         gameoverPanel.alpha = 0;
         gameoverPanel.blocksRaycasts = false;
     }
-
-    void Update()
-    {
-        
-    }
-
 
     public void OpenPanel(bool on)
     {
