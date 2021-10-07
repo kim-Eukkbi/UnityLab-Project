266aec8901494029e907fb15d90134996632a186
JUGU <qheorjsdnr@naver.com>
2021-10-07 20:09:26 +0900

    맵 랜덤 생성

--- Assets/Scripts/etc/MapManager.cs
+++ Assets/Scripts/etc/MapManager.cs
@@ -9,12 +9,14 @@ public class MapManager : MonoBehaviour
     public static MapManager instance;
 
     const int START_SIZE = 5;
-    public Pool<Map> pool;
+    public Pool<Map> pool1;
+    public Pool<Map> pool2;
+    public Pool<Map> pool3;
+    public Pool<Map> pool4;
+    public Pool<Map> pool5;
     public Dictionary<int,Map> mapDic;
     [SerializeField]
-    private GameObject mapObj;
-    [SerializeField]
-    private GameObject plane;
+    private List<GameObject> mapObjList = new List<GameObject>();
 
     public PlayerController pc;
 
@@ -35,17 +37,15 @@ public class MapManager : MonoBehaviour
     void Start()
     {
         pc = GameObject.Find("Player").GetComponent<PlayerController>();
-        rend = plane.GetComponent<MeshRenderer>();
+
         lastTrm = pc.transform;
-        pool = new Pool<Map>(new PrefabFactory<Map>(mapObj), START_SIZE);
-        for (int i = 0; i < pool.members.Count; i++)
-        {
-            pool.members[i].gameObject.SetActive(false);
-        }
-        for (int i = 0; i < 10; i++)
-        {
-            Spawn();
-        }
+        pool1 = new Pool<Map>(new PrefabFactory<Map>(mapObjList[0]), START_SIZE);
+        pool2 = new Pool<Map>(new PrefabFactory<Map>(mapObjList[1]), START_SIZE);
+        pool3 = new Pool<Map>(new PrefabFactory<Map>(mapObjList[2]), START_SIZE);
+        pool4 = new Pool<Map>(new PrefabFactory<Map>(mapObjList[3]), START_SIZE);
+        pool5 = new Pool<Map>(new PrefabFactory<Map>(mapObjList[4]), START_SIZE);
+       
+        
        
     }
 
@@ -59,13 +59,41 @@ public class MapManager : MonoBehaviour
 
     void Spawn()
     {
-        Map map = pool.Allocate();
+        int idx = UnityEngine.Random.Range(0,mapObjList.Count - 1);
+        Map map = pool1.Allocate();
+        switch (idx)
+        {
+            case 0:
+                map = pool1.Allocate();
+                break;
+            case 1:
+                map = pool2.Allocate();
+                break;
+            case 2:
+                map = pool3.Allocate();
+                break;
+            case 3:
+                map = pool4.Allocate();
+                break;
+            case 4:
+                map = pool5.Allocate();
+                break;
+        }
 
         EventHandler handler = null;
+        Transform[] trms = map.gameObject.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < trms.Length; i++)
+        {
+            if (trms[i].gameObject.CompareTag("Ground")) 
+            {
+                rend = trms[i].GetComponent<MeshRenderer>();
+            }
+        }
 
         if (mapCount == 0) 
         {
-            map.transform.position = new Vector3(pc.transform.transform.position.x, pc.transform.transform.position.y, pc.transform.transform.position.z);
+            map.transform.position = new Vector3(lastTrm.transform.position.x, lastTrm.transform.position.y, pc.transform.transform.position.z);
 
         }
         else
@@ -75,7 +103,7 @@ public class MapManager : MonoBehaviour
 
         // 생성시 사망 처리용 Death 이벤트핸들러를 직접 정의 (Lambda식 구현)
         handler = (sender, e) => {
-            pool.Release(map);
+            //pool.Release(map);
             map.Death -= handler;
             mapCount--;
         };
